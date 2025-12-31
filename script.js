de5d5342652b8020ad2c1626bf4ea177677fd1ec
Ayushi Paladiya <ayushi.paladiya@shunyavkash.com>
2025-12-31 14:07:34 +0530

    Updated code

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const options = document.querySelectorAll(".card-option");
   const rows = document.querySelectorAll("tbody tr");
 
-  form.addEventListener("click", (data) => {
-    data.stopPropagation();
+  form?.addEventListener("click", (e) => {
+    e.stopPropagation();
     list.classList.toggle("active");
   });
 
   options.forEach((opt) => {
-    opt.addEventListener("click", (data) => {
-      data.stopPropagation();
+    opt.addEventListener("click", (e) => {
+      e.stopPropagation();
 
       const count = Number(opt.textContent);
       text.textContent = opt.textContent;
@@ -26,5 +26,25 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".tab").forEach(initTabs);
 
+  function initTabs(tab) {
+    const tabs = tab.querySelectorAll(":scope > .tab-list .tab-item");
+    const contents = tab.querySelectorAll(
+      ":scope > .tab-content > .tab-content-item"
+    );
 
+    tabs.forEach((btn, index) => {
+      btn.addEventListener("click", () => {
+        tabs.forEach((t) => t.classList.remove("active"));
+        contents.forEach((c) => c.classList.remove("active"));
+
+        btn.classList.add("active");
+        contents[index].classList.add("active");
+      });
+    });
+
+    tabs[0]?.click();
+  }
+});
